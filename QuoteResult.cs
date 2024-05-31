519d37489c89afbcac9f8c3cc4093b089e23fc42
Talita Beneves <talitabeneves15@gmail.com>
2024-05-31 02:37:43 -0300

    QuoteResult para desserialização do Json

---
+++ QuoteResult.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace StockQuoteAlert
+{
+    public class QuoteResult
+    {
+        public class Quote
+        {
+            [JsonPropertyName("results")]
+            public List<Result> Results { get; set; } = new List<Result>();
+        }
+
+        public class Result
+        {
+            [JsonPropertyName("regularMarketPrice")]
+            public decimal RegularMarketPrice { get; set; }
+        }
+    }
+}
