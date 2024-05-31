fa3e1c5c18cc60ffcd975f80183c81238ca43543
Talita Beneves <talitabeneves15@gmail.com>
2024-05-30 22:26:16 -0300

    desserializando e aplicando configurações do arquivo JSON

---
+++ EmailConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+
+namespace StockQuoteAlert
+{
+    public class EmailConfig
+    {
+        public string EmailRecipient { get; set; } = string.Empty;
+        public string SmtpServer { get; set; } = string.Empty;
+        public int SmtpPort { get; set; }
+        public string SmtpUser { get; set; } = string.Empty;
+        public string SmtpPassword { get; set; } = string.Empty;
+
+        public static EmailConfig Read(string path)
+        {
+            string configJson = File.ReadAllText(path);
+            try
+            {
+                return JsonSerializer.Deserialize<EmailConfig>(configJson)!;
+            }
+            catch (JsonException ex)
+            {
+                throw new Exception("Error deserializing Json: " + ex.Message);
+            }
+           
+        }
+    }
+}
