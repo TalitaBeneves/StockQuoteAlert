60d951fd55579fffe0ee6b31c45621069acfcaa5
Talita Beneves <talitabeneves15@gmail.com>
2024-05-31 02:37:10 -0300

    buscando a cotação do ativo

---
+++ QuoteService.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+using static StockQuoteAlert.QuoteResult;
+
+namespace StockQuoteAlert
+{
+    public class QuoteService
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private readonly string Token = "5NnutG1JSfpN3zFRUahCqc";
+
+        public async Task<Quote> GetQuote(string stock)
+        {
+            string url = $"https://brapi.dev/api/quote/{stock}?token={Token}";
+
+            HttpResponseMessage response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            var responseBody = await response.Content.ReadAsStringAsync();
+            
+            try
+            {
+                return JsonSerializer.Deserialize<Quote>(responseBody)!;
+            }
+            catch (JsonException ex)
+            {
+                throw new Exception("Error deserializing Json: " + ex.Message);
+            }
+
+        }
+    }
+}
