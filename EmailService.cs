19377b265eecfb5ea020429a9c925594c62fd133
Talita Beneves <talitabeneves15@gmail.com>
2024-05-31 02:41:05 -0300

    EmailService lógica para o envio de email

---
+++ EmailService.cs
@@ -0,0 +1,67 @@
+﻿using System.Net.Mail;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace StockQuoteAlert
+{
+    public class EmailService
+    {
+        private readonly EmailConfig _config;
+        public EmailService(EmailConfig config)
+        {
+            _config = config;
+        }
+
+        public void SendEmail(string stock_asset, string action, decimal price, string message)
+        {
+            var msg = PrepareteMessage(stock_asset, action, price, message);
+            EnviarEmailPorSMTP(msg);
+        }
+
+        private MailMessage PrepareteMessage(string stock_asset, string action, decimal price, string message)
+        {
+            var mail = new MailMessage();
+            mail.From = new MailAddress(_config.SmtpUser);
+
+            if (validarEmail(_config.EmailRecipient))
+                mail.To.Add(_config.EmailRecipient);
+
+            mail.Subject = $"Alerta de {action} {stock_asset}";
+            mail.Body = message;
+
+            mail.IsBodyHtml = true;
+
+            return mail;
+        }
+
+        private bool validarEmail(string email)
+        {
+            Regex reg = new Regex(@"\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}");
+            if (reg.IsMatch(email))
+                return true;
+
+            return false;
+        }
+
+        private void EnviarEmailPorSMTP(MailMessage message)
+        {
+            try
+            {
+                SmtpClient smtp = new SmtpClient(_config.SmtpServer);
+                smtp.Port = _config.SmtpPort;
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.Credentials = new NetworkCredential(_config.SmtpUser, _config.SmtpPassword);
+                smtp.EnableSsl = true;
+                smtp.Send(message);
+                smtp.Dispose();
+
+                Console.WriteLine("Email sent successfully");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error sending email {ex.Message}");
+            }
+
+        }
+    }
+}
