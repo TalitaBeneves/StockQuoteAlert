bb4d5d0036bd7d5584f2c737476a55a001b3debc
Talita Beneves <talitabeneves15@gmail.com>
2024-05-30 22:32:10 -0300

    pegando o arquivo json e lendo

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StockQuoteAlert;
+using System;
 
 class Program
 {
@@ -6,8 +7,8 @@ class Program
     {
         if (args.Length != 3)
         {
-            Console.WriteLine("Usage: Go to the bin > Debug > net8 folder \n and type: dotnet StockQuoteAlert.dll <stock> <selling_price> <buying_price>");
-            return;
+            Console.WriteLine("Usage: Go to the bin > Debug > net8.0 folder \n and type: dotnet StockQuoteAlert.dll <stock> <selling_price> <buying_price>");
+            args = new string[] { "AAPL", "150", "100" };
         }
 
         string stock_assets = args[0];
@@ -19,7 +20,18 @@ class Program
             return;
         }
 
-      
+        string projectDirectory = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\..\..\"));
+
+        string configPath = Path.Combine(projectDirectory, "Configs.json");
+        if (!File.Exists(configPath))
+        {
+            Console.WriteLine("Configuration file not found.");
+            return;
+        }
+
+        EmailConfig config = EmailConfig.Read(configPath);
+        Console.WriteLine($"EmailRecipient: {config.EmailRecipient}");
+
         Console.WriteLine($"Stock: {stock_assets}");
         Console.WriteLine($"Selling price: {sellingPrice}");
         Console.WriteLine($"Buying price: {buyingPrice}");
